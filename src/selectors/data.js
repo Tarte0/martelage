99886b0814d3adc6541bfe6ec5c2c5c456cf012f
pernifloss <pernifloss@gmail.com>
2018-02-20 13:13:15 +0100

    add test (#1)

--- src/selectors/data.js
+++ src/selectors/data.js
@@ -1,30 +1,34 @@
-import { createSelector } from 'reselect'
-const getParcels = state => state.getIn(['data','parcels']);
-const getSelectedParcel = state => state.getIn(['data','selectedParcel']);
+import {createSelector} from 'reselect'
+const getParcels = state => state.getIn(['data', 'parcels']);
+const getSelectedParcel = state => state.getIn(['data', 'selectedParcel']);
 
 export const selectParcelsAsArray = createSelector(
     [getParcels],
     (parcels) => {
-        return Object.keys(parcels.toJS()).map(id=> ({id , ...parcels.get(id).toJS() }))
+        return Object.keys(parcels.toJS()).map(id => ({id, ...parcels.get(id).toJS()}))
     }
 );
 
 export const selectedParcel = createSelector(
     [getParcels, getSelectedParcel],
     (parcels, selectedParcelId) => {
-        if(selectedParcelId){
+        if (selectedParcelId) {
             return parcels.get(selectedParcelId);
         }
         return null;
     }
 );
 
-export const getSelectedTree = createSelector(
+export const getSelectedTrees = createSelector(
     [getParcels, getSelectedParcel],
     (parcels, selectedParcelId) => {
-        if(selectedParcelId){
-            return parcels.getIn([selectedParcelId,selectedParcelId.arbres.]);
+        if (selectedParcelId) {
+            const treesObject = parcels.getIn([selectedParcelId, 'arbres']).toJS();
+            return Object.keys(treesObject).map((e) => {
+                treesObject[e].id = e;
+                return treesObject[e];
+            });
         }
-        return null;
+        return [];
     }
 );
